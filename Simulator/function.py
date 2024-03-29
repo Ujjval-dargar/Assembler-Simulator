d7eac44cf3fe4fe03f31467f75a3d30f80febb55
Ujjval-dargar <ujjval23564@iiitd.ac.in>
2024-03-29 13:12:50 +0530

    S_type fixed

--- Simulator/function.py
+++ Simulator/function.py
@@ -1,17 +1,31 @@
-from constants import *
+from constants import * 
 
-def sext(line,bits):
-    if line[0]==0:
-        line=(bits-len(line))*'0'+line
+def sext(line, bits):
+    if line[0] == '0':
+        line = (bits-len(line))*'0'+line
     else:
-        line=(bits-len(line))*'1'+line
+        line = (bits-len(line))*'1'+line
     return line
 
 
 def S_type(line):
-    opcode=line[-7:]
-    imm=line[7:11+1]+line[25:31+1]
-    rs1=line[15:19+1]
-    rs2=line[20:25+1]
-    mem=register_value[Address_Register[rs1]]+sext(imm,32)
-    data_memory[mem]=register_value[Address_Register[rs2]]
+    opcode = line[-7:]
+
+    funct3 = line[-15:-12]
+
+    rs1 = line[-20:-15]
+    rs2 = line[-25:-20]
+
+    imm = line[-12:-7]+line[-32:-25]
+
+    s = sext(imm, 32)
+
+    mem = register_value[Address_Register[rs1]] + sext(imm, 32)
+
+    # data_memory[mem] = register_value[Address_Register[rs2]]
+
+
+ins = "00000010011000010010000000100011"
+# ins="0000 0010 0110 00010 010 00000 0100011"
+print(ins)
+S_type(ins)
