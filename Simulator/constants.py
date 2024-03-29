eac8a692c6c81b95e46c751dfdbab3f70d50242a
Pratyush-Gangwar <pratyush23395@iiitd.ac.in>
2024-03-29 11:25:07 +0530

    Update constants.py

--- Simulator/constants.py
+++ Simulator/constants.py
@@ -1,3 +1,223 @@
+MNEMONICS_DICT = {
+
+    # R-TYPE MNEMONICS
+    "add": {
+        "type": "R",
+        "funct3": "000",
+        "funct7": "0000000",
+        "opcode": "0110011",
+        "textSyntax": ["REG", "REG", "REG"]
+    },
+
+    "sub": {
+        "type": "R",
+        "funct3": "000",
+        "funct7": "0100000",
+        "opcode": "0110011",
+        "textSyntax": ["REG", "REG", "REG"]
+    },
+
+    "sll": {
+        "type": "R",
+        "funct3": "001",
+        "funct7": "0000000",
+        "opcode": "0110011",
+        "textSyntax": ["REG", "REG", "REG"]
+    },
+
+    "slt": {
+        "type": "R",
+        "funct3": "010",
+        "funct7": "0000000",
+        "opcode": "0110011",
+        "textSyntax": ["REG", "REG", "REG"]
+
+    },
+
+    "sltu": {
+        "type": "R",
+        "funct3": "011",
+        "funct7": "0000000",
+        "opcode": "0110011",
+        "textSyntax": ["REG", "REG", "REG"]
+    },
+
+    "xor": {
+        "type": "R",
+        "funct3": "100",
+        "funct7": "0000000",
+        "opcode": "0110011",
+        "textSyntax": ["REG", "REG", "REG"]
+    },
+
+    "srl": {
+        "type": "R",
+        "funct3": "101",
+        "funct7": "0000000",
+        "opcode": "0110011",
+        "textSyntax": ["REG", "REG", "REG"]
+    },
+
+    "or": {
+        "type": "R",
+        "funct3": "110",
+        "funct7": "0000000",
+        "opcode": "0110011",
+        "textSyntax": ["REG", "REG", "REG"]
+    },
+
+    "and": {
+        "type": "R",
+        "funct3": "111",
+        "funct7": "0000000",
+        "opcode": "0110011",
+        "textSyntax": ["REG", "REG", "REG"]
+    },
+
+    # I-TYPE MNEMONICS
+    "lw": {
+        "type": "I",
+        "opcode": "0000011",
+        "funct3": "010",
+        "funct7": None,
+        "textSyntax": ["REG", "REG", "IMM"]
+    },
+
+    "addi": {
+        "type": "I",
+        "opcode": "0010011",
+        "funct3": "000",
+        "funct7": None,
+        "textSyntax": ["REG", "REG", "IMM"]
+    },
+
+    "sltiu": {
+        "type": "I",
+        "opcode": "0010011",
+        "funct3": "011",
+        "funct7": None,
+        "textSyntax": ["REG", "REG", "IMM"]
+    },
+
+    "jalr": {
+        "type": "I",
+        "opcode": "1100111",
+        "funct3": "000",
+        "funct7": None,
+        "textSyntax": ["REG", "REG", "IMM"]
+    },
+
+    # U-TYPE MNEMONICS
+    "auipc": {
+        "type": "U",
+        "opcode": "0010111",
+
+        "funct3": None,
+        "funct7": None,
+
+        "textSyntax": ["REG", "IMM"]
+
+    },
+
+    "lui": {
+        "type": "U",
+        "opcode": "0110111",
+
+        "funct3": None,
+        "funct7": None,
+
+        "textSyntax": ["REG", "IMM"]
+
+    },
+
+    # J-TYPE MNEMONICS
+    "jal": {
+        "type": "J",
+        "opcode": "1101111",
+
+        "funct3": None,
+        "funct7": None,
+
+        "textSyntax": ["REG", "IMM"]
+
+    },
+
+    # S-TYPE MNEMONICS
+    "sw": {
+        "type": "S",
+        "opcode": "0100011",
+        "funct3": "010",
+        "funct7": None,
+        "textSyntax": ["REG", "IMM", "REG"]
+    },
+
+    # B-TYPE MNEMONICS
+    "beq": {
+        "type": "B",
+        "opcode": "1100011",
+        "funct3": "000",
+        "funct7": None,
+        "textSyntax": ["REG", "REG", "IMM"]
+    },
+
+    "bne": {
+        "type": "B",
+        "opcode": "1100011",
+        "funct3": "001",
+        "funct7": None,
+        "textSyntax": ["REG", "REG", "IMM"]
+    },
+
+    "blt": {
+        "type": "B",
+        "opcode": "1100011",
+        "funct3": "100",
+        "funct7": None,
+        "textSyntax": ["REG", "REG", "IMM"]
+    },
+
+    "bge": {
+        "type": "B",
+        "opcode": "1100011",
+        "funct3": "101",
+        "funct7": None,
+        "textSyntax": ["REG", "REG", "IMM"]
+    },
+
+    "bltu": {
+        "type": "B",
+        "opcode": "1100011",
+        "funct3": "110",
+        "funct7": None,
+        "textSyntax": ["REG", "REG", "IMM"]
+    },
+
+    "bgeu": {
+        "type": "B",
+        "opcode": "1100011",
+        "funct3": "111",
+        "funct7": None,
+        "textSyntax": ["REG", "REG", "IMM"]
+    }
+
+}
+
+
+OPCODE_DICT = {'0110011': {'0110011': {'type': 'R', 'funct3': '111', 'funct7': '0000000', 'textSyntax': ['REG', 'REG', 'REG'], 'mnemonic': 'and'}}, '0000011': {'0000011': {'type': 'I', 'funct3': '010', 'funct7': None, 'textSyntax': ['REG', 'REG', 'IMM'], 'mnemonic': 'lw'}}, '0010011': {'0010011': {'type': 'I', 'funct3': '011', 'funct7': None, 'textSyntax': ['REG', 'REG', 'IMM'], 'mnemonic': 'sltiu'}}, '1100111': {'1100111': {'type': 'I', 'funct3': '000', 'funct7': None, 'textSyntax': ['REG', 'REG', 'IMM'], 'mnemonic': 'jalr'}}, '0010111': {'0010111': {'type': 'U', 'funct3': None, 'funct7': None, 'textSyntax': ['REG', 'IMM'], 'mnemonic': 'auipc'}}, '0110111': {'0110111': {'type': 'U', 'funct3': None, 'funct7': None, 'textSyntax': ['REG', 'IMM'], 'mnemonic': 'lui'}}, '1101111': {'1101111': {'type': 'J', 'funct3': None, 'funct7': None, 'textSyntax': ['REG', 'IMM'], 'mnemonic': 'jal'}}, '0100011': {'0100011': {'type': 'S', 'funct3': '010', 'funct7': None, 'textSyntax': ['REG', 'IMM', 'REG'], 'mnemonic': 'sw'}}, '1100011': {'1100011': {'type': 'B', 'funct3': '111', 'funct7': None, 'textSyntax': ['REG', 'REG', 'IMM'], 'mnemonic': 'bgeu'}}}
+
+def generate_dict():
+    for mnemonic, mnemonic_dict in MNEMONICS_DICT.items():
+        opcode_str = mnemonic_dict["opcode"]
+
+        mnemonic_dict.pop("opcode")
+        mnemonic_dict["mnemonic"] = mnemonic
+
+        new_dict = { opcode_str :  mnemonic_dict  }
+        OPCODE_DICT[opcode_str] = new_dict
+
+generate_dict()
+print(OPCODE_DICT)
+
 
 Address_Register = {
 
